17f1e861ca145fa7d7048ec6d0728f2a286c5ffb
Hannyer <57728626+Hannyer@users.noreply.github.com>
2021-03-11 22:43:36 -0600

    Nuevo

---
+++ src/java/Controller/beanProducto.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import Model.Product;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author Usuario
+ */
+@Named(value = "beanProducto")
+@SessionScoped
+public class beanProducto implements Serializable {
+
+  
+    
+    
+    public beanProducto() {
+    }
+    
+    
+
+    
+    
+    
+    
+}
